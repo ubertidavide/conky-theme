23a15fe308715f79301ee592903061e6a24e1e3b
Davide Uberti <24529587+ubertidavide@users.noreply.github.com>
2024-08-24 11:28:12 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 sudo apt-get update && sudo apt-get upgrade -y
-sudo apt install -y conky-all sensors
-cp conky.conf /etc/conky/conky.conf
-cp conky.desktop ~/.config/autostart/conky.desktop
+sudo apt install -y conky-all
+sudo cp conky.conf /etc/conky/conky.conf
+sudo cp conky_functions.lua /etc/conky/conky_functions.lua
+sudo cp conky.desktop ~/.config/autostart/conky.desktop
